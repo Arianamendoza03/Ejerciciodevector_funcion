e34b577a2c3a63f4c50462188eea2725f788bf2a
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-08-02 23:21:26 +1200

    VECTORES

---
+++ cmake-build-debug/CMakeFiles/Ejercicio_de__Vector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio_de__Vector.dir/main.cpp.obj"
+  "Ejercicio_de__Vector.exe"
+  "Ejercicio_de__Vector.exe.manifest"
+  "Ejercicio_de__Vector.pdb"
+  "libEjercicio_de__Vector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio_de__Vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
